4a2b9972b3f5578f341048ac7e04bacc6c05c96c
xxria17 <xxrcy32@gmail.com>
2021-03-11 15:46:15 +0900

    2주차 3차시

--- 01/app.js
+++ 01/app.js
@@ -32,3 +32,54 @@ var log = function(str) {
 
 log('hello');
 */
+
+// 3차시
+
+/*
+var image = {
+    width : 100,
+    heigth : 100,
+    name: '1.png',
+};
+
+var image1 = [1, 2, 3, 4, 5, 6, 'a', {}];
+image1[0] = 'a';
+
+
+// var imageWidth = 100;
+// var imageHeigth = 100;
+// var imageName = '1.png';
+console.log(image, image1);
+*/
+
+/*
+var data = null; //null
+var data1 = ''; //''
+var data2;  //undefined
+
+console.log(data, data1, data2);
+*/
+
+/*
+console.log(1 + 2);
+console.log('a' + 'b');
+console.log(1 + '4');
+
+var count = 1;
+var number = '2';
+console.log(count, number);
+
+console.log('' == '0');
+console.log(0 === '', 0 == '');
+
+var isHero = true;
+if (!isHero) {
+    console.log('hero');
+}
+
+console.log(typeof 1);
+console.log(typeof []);
+console.log(typeof '1');
+console.log(typeof {});
+
+*/
