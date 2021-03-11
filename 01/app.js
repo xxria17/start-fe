1f1e19d7189c4d59f08512b01889696fbcec46b4
xxria17 <xxrcy32@gmail.com>
2021-03-11 15:20:56 +0900

    2주차 2차시

---
+++ 01/app.js
@@ -0,0 +1,34 @@
+console.log('app.js');
+
+/*
+var name = '이도현';
+var Name = '이도현';
+
+console.log(name);
+console.log(Name);
+*/
+
+// console.log(1);
+// console.log(2);
+
+/* var count = 1;
+console.log(count);
+*/
+
+/*
+var isMan = true; // boolean
+var isMan = "true"; //String
+
+console.log(isMan);
+*/
+
+/*function log(str) {
+    console.log(str);
+}
+
+var log = function(str) {
+    console.log(str);
+}
+
+log('hello');
+*/
