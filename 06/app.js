9a3151ebeb513fc6358d6752e290615581bc5374
xxria17 <xxrcy32@gmail.com>
2021-04-12 18:27:02 +0900

    6주차 1차시

---
+++ 06/app.js
@@ -0,0 +1,41 @@
+// let a = 'aa';
+// const b = 'bb';
+// var c = '11';
+// c = 33;
+// b = 11;
+// console.log(a);
+// console.log(b);
+
+
+// var str = 'aaaaaaa \
+//     sssdfsd';
+// const s =`aaaa
+//     ccc
+//     ddd`;
+// console.log(str);
+// console.log(s);
+
+// function sum(x, y = 1) {
+//     return x + y;
+// }
+//
+// console.log(sum(1));
+
+
+// const arr = [1, 2, 3];
+//
+// const newArr = arr.map((item) => item + 1);
+// console.log(newArr);
+//
+// document
+//     .getElementById('box')
+//     .addEventListener('click', event => console.log(1));
+//
+// function sum(x,y,z) {
+//     return x + y+ z;
+// }
+//
+// const numbers = [1,2,3];
+// console.log(sum(...numbers));
+// console.log(sum(1,2,3));
+
