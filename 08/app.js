fede2a8397dbde6a3fc98d8d014647e9d01126ee
xxria17 <xxrcy32@gmail.com>
2021-05-03 14:49:07 +0900

    9주차

---
+++ 08/app.js
@@ -0,0 +1,16 @@
+
+// const promise = fetch('./json.json');
+// console.log(promise);
+//
+// promise.then(res => {
+//     console.log('res: ', res);
+//     // const textPromise = res.text();
+//     const textPromise = res.json();
+//     console.log('textPromise: ', textPromise);
+//
+//     textPromise.then(data => console.log('data : ', data));
+// });
+
+function callback(data) {
+    console.log('callback:', data);
+}
